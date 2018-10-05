33b1f90a14dd9d1d3fd7ddc6d582e828643cbadb
Kim,Donghyun <d_k067@student.usc.edu.au>
2018-10-05 23:05:18 +1000

    finally, I was able to click the crime and move to next stage
    I missed some codes......

--- app/src/main/java/com/example/yohan/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/yohan/criminalintent/CrimeActivity.java
@@ -13,14 +13,7 @@ import java.util.UUID;
 //FragmentActivity
 public class CrimeActivity extends SingleFragmentActivity {
 
-    public static final String EXTRA_CRIME_ID =
-            "com.bignerdranch.android.criminalintent.crime_id";
 
-    public static Intent newIntent(Context packageContext, UUID crimeId) {
-        Intent intent = new Intent(packageContext, CrimeActivity.class);
-        intent.putExtra(EXTRA_CRIME_ID, crimeId);
-        return intent;
-    }
 
     @Override
     protected Fragment createFragment() {
